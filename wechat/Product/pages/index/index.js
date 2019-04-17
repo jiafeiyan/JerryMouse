e84db64ca7c6c407608504b4646eb5552e19b124
代思豪 <798814330@qq.com>
2019-04-17 19:22:40 +0800

    微信端项目初始化
    1、轮播图
    2、使用iconfont图标库,项目地址为//at.alicdn.com/t/font_1143309_10jl1qvfoiwh.css
    3、优惠券模板

---
+++ wechat/Product/pages/index/index.js
@@ -0,0 +1,78 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    carouselList: [{
+        src: "/images/product/banner3.jpg",
+        background: "rgb(203, 87, 60)",
+      },
+      {
+        src: "/images/product/banner2.jpg",
+        background: "rgb(205, 215, 218)",
+      },
+      {
+        src: "/images/product/banner4.jpg",
+        background: "rgb(183, 73, 69)",
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
