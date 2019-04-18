bfb2943bdf5f41975ab7c07f99732b18e3eaf5af
代思豪 <798814330@qq.com>
2019-04-18 16:16:45 +0800

    添加底部图片，修改部分样式

--- wechat/Product/pages/product_catalog/index.js
+++ wechat/Product/pages/product_catalog/index.js
@@ -1,12 +1,14 @@
 // pages/product_catalog/index.js
+var sliderWidth = 60; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tabs: ["选项一", "选项二", "选项三"],
-    activeIndex: 1,
+    tabs: ["阳台窗", "推拉门", "厨房门", "平开门", "窗", "阳光房"],
+    activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0
   },
