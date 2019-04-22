a77ad97fc66d3090f1d4d1baf6c4878a2b1fe605
代思豪 <798814330@qq.com>
2019-04-22 19:26:43 +0800

    完成my界面

--- wechat/Product/pages/my/index.js
+++ wechat/Product/pages/my/index.js
@@ -6,6 +6,7 @@ Page({
   data: {
     hasUserInfo: true,
     userInfo: null,
+    distance:0,
   },
   userAuthorized() {
     wx.getSetting({
@@ -40,64 +41,103 @@ Page({
   onJumpToAbout() {
 
   },
+  goto() {
+    // wx.getLocation({
+    //   success: function(res) {},
+    // })
+    wx.openLocation({
+      latitude: 30.5061230000,
+      longitude: 114.3992030000,
+      scale: 12,
+      name: "光谷广场",
+      address: "光谷广场地铁站"
+    })
+  },
+  call() {
+    wx.makePhoneCall({
+      phoneNumber: '18510097098',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var that = this
     this.userAuthorized()
     wx.getUserInfo({
       success: data => {
 
       }
     })
+    wx.getLocation({
+      type: "gcj02",
+      success: function(res) {
+        let distance = that.distance(res.latitude, res.longitude, 30.5061230000, 114.3992030000);
+        that.setData({
+          distance: distance
+        })
+      },
+    })
+  },
+
+  distance: function(la1, lo1, la2, lo2) {
+    var La1 = la1 * Math.PI / 180.0;
+    var La2 = la2 * Math.PI / 180.0;
+    var La3 = La1 - La2;
+    var Lb3 = lo1 * Math.PI / 180.0 - lo2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(La3 / 2), 2) + Math.cos(La1) * Math.cos(La2) * Math.pow(Math.sin(Lb3 / 2), 2)));
+    s = s * 6378.137;
+    s = Math.round(s * 10000) / 10000;
+    s = s.toFixed(2);
+    return s;
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
