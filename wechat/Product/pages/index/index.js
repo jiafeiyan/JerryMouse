3f893825a059dbd21b6e31fdddaaa46cb4855b57
代思豪 <798814330@qq.com>
2019-04-22 11:00:35 +0800

    添加mini-coupon组件，优化部分样式

--- wechat/Product/pages/index/index.js
+++ wechat/Product/pages/index/index.js
@@ -29,9 +29,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-      productModel.getAllProduct().then(res=>{
-        console.log("res is ===" + res);
-      })
+    
   },
 
   /**
