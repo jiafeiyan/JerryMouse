533d871884f9c5204bc849c18a75b13f0cad7e40
代思豪 <798814330@qq.com>
2019-04-22 20:00:44 +0800

    brand样式

--- wechat/Product/pages/brand/index.js
+++ wechat/Product/pages/brand/index.js
@@ -5,9 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    aboutme:'/images/brand/aboutme.jpeg'
   },
 
+  call() {
+    wx.makePhoneCall({
+      phoneNumber: '18510097098',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
